c5f74baf971fdde7680faabeaed0237d74a29abf
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-25 17:18:38 +0300

    Add MenuItem model and create migrations

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    """
+    Модель для хранения элементов меню.
+    """
+    name = models.CharField(max_length=100, help_text="Название элемента меню")
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children',
+                               help_text="Родительский элемент меню (пусто для корневых элементов)")
+    url = models.CharField(max_length=100, help_text="URL элемента меню")
+
+    def __str__(self):
+        return self.name
