e9012432153c5f2434333c177ba545c9586dc395
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-25 17:23:28 +0300

    Add admin interface for managing menu items

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MenuItem
 
-# Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    """
+    Класс для настройки административной панели меню.
+    """
+    list_display = ['name', 'url', 'parent']
+    search_fields = ['name', 'url']
+
+    filter_horizontal = ('parent',)  # Используйте filter_horizontal для выбора родительских элементов.
